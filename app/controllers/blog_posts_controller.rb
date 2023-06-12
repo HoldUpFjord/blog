a62a9b82a7d2dd48794995cf8ce73415bed3b8db
jackmonaco20 <jackmonaco20@yahoo.com>
2023-06-12 14:08:23 -0400

    patch/edits now work

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,7 +8,6 @@ class BlogPostsController < ApplicationController
   end
 
   def show
-    binding.pry
     # params comes from action controller
     @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
@@ -33,6 +32,19 @@ class BlogPostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+        redirect_to @blog_post
+    else
+        render :edit, status: :unprocessable_entity
+    end
+   end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post 
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-       #route^^^       #control^ | action^
-       
-       #blog_post_path(1) -> "blog_posts/1"
-       #blog_post_url(1) -> www.disco.com/blogs/blog/1
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  get '/blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
+  get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
+  # route^^^       # control^ | action^
+  patch '/blog_posts/:id', to: 'blog_posts#update'
+  get '/blog_posts/:id/edit', to: 'blog_posts#edit', as: :edit_blog_post
+  # blog_post_path(1) -> "blog_posts/1"
+  # blog_post_url(1) -> www.disco.com/blogs/blog/1
+  post '/blog_posts', to: 'blog_posts#create', as: :blog_posts
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
